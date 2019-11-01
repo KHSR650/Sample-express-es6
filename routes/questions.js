d8bc8b87f35e7703370acf1704de0e69aea308e9
SUNDAR <khsr650.cse@gmail.com>
2019-11-01 09:44:13 +0530

    no changeds

--- routes/questions.js
+++ routes/questions.js
@@ -2,6 +2,8 @@ import express from "express";
 import questionsControllers from "../controllers/questionsControllers"
 const router = express.Router();
 /* GET home page. */
+
+// ntgs
 router.get("/", questionsControllers.listAllQuestions);
 router.post("/", questionsControllers.addQuestion);
 router.get("/search", questionsControllers.searchQuestion);
